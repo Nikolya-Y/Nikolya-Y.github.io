d754d8bbf173f49d5b3c44337df6f93ff31dd11b
Nikolya.Y <46538947+Nikolya-Y@users.noreply.github.com>
2020-01-30 23:04:55 +0200

    another commit

--- site4____Univercity_VR_Project/script.js
+++ site4____Univercity_VR_Project/script.js
@@ -1,22 +1,42 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-  });
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel();
+//   });
 
 
-$('.owl-carousel').owlCarousel({
-    items:1,
-    merge:true,
-    loop:true,
-    margin:10,
-    video:true,
-    lazyLoad:true,
-    center:true,
-    responsive:{
-        480:{
-            items:2
-        },
-        600:{
-            items:4
-        }
-    }
-})
+// $('.owl-carousel').owlCarousel({
+//     items:1,
+//     merge:true,
+//     loop:true,
+//     margin:10,
+//     video:true,
+//     lazyLoad:true,
+//     center:true,
+//     responsive:{
+//         480:{
+//             items:2
+//         },
+//         600:{
+//             items:4
+//         }
+//     }
+// })
+
+
+
+// скрипт управления видео
+
+// // загрузка видео по йди с определённой секунды и в каком-то там качестве
+// loadVideoById("bHQqvYy5KYo", 5, "large");
+// //запуск видео и уже с секунды указанной выше
+// player.playVideo():Void;
+
+
+
+//считываение кнопок управления перелистыванимем видео
+function myFunc (ev)
+{
+var e = window.event || ev, K = e.keyCode;
+     if (K == 37) alert ('Нажата ЛЕВАЯ стрелка');
+else if (K == 39) alert ('Нажата ПРАВАЯ стрелка');
+}
+
