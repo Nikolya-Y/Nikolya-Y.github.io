b1fca42b56ec3ed06a606e6d55ebc05e7fd778fd
Nikolya.Y <46538947+Nikolya-Y@users.noreply.github.com>
2020-02-13 15:28:33 +0200

    Update script.js

--- site4____Univercity_VR_Project/script.js
+++ site4____Univercity_VR_Project/script.js
@@ -1,16 +1,11 @@
 $(document).ready(function(){
      $('.owl-carousel').owlCarousel({
           items:1,
-          merge:true,
           loop:true,
-          margin:10,
           video:true,
           center:true
       });
 });
-
-alert(aria-valuetext);
-
 
 
 
