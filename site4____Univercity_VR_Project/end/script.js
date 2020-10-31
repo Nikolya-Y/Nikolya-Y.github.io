ed2696c1f055d3c758324805abcaca1c9b4cf420
Nikolya.Y <46538947+Nikolya-Y@users.noreply.github.com>
2020-10-31 17:51:55 +0200

    first true work version
    first end

---
+++ site4____Univercity_VR_Project/end/script.js
@@ -0,0 +1,118 @@
+var youTubePlayer;
+
+function onYouTubeIframeAPIReady() {
+    'use strict';
+
+    var inputVideoId = document.getElementById('YouTube-video-id');
+    var videoId = inputVideoId.value;
+    var suggestedQuality = 'tiny';
+    var height = 600;
+    var width = 1066.67;
+    var youTubePlayerVolumeItemId = 'YouTube-player-volume';
+
+
+    function onError(event) {
+        youTubePlayer.personalPlayer.errors.push(event.data);
+    }
+
+
+    function onReady(event) {
+        var player = event.target;
+
+        player.loadVideoById({suggestedQuality: suggestedQuality,
+                              videoId: videoId
+                             });
+        player.pauseVideo();
+        youTubePlayerDisplayFixedInfos();
+    }
+
+
+    function onStateChange(event) {
+        var volume = Math.round(event.target.getVolume());
+        var volumeItem = document.getElementById(youTubePlayerVolumeItemId);
+
+        if (volumeItem && (Math.round(volumeItem.value) != volume)) {
+            volumeItem.value = volume;
+        }
+    }
+
+
+    youTubePlayer = new YT.Player('YouTube-player',
+                                  {videoId: videoId,
+                                   height: height,
+                                   width: width,
+                                   playerVars: {'autohide': 0,
+                                                'cc_load_policy': 0,
+                                                'controls': 2,
+                                                'disablekb': 1,
+                                                'iv_load_policy': 3,
+                                                'modestbranding': 1,
+                                                'rel': 0,
+                                                'showinfo': 0,
+                                                'start': 3
+                                               },
+                                   events: {'onError': onError,
+                                            'onReady': onReady,
+                                            'onStateChange': onStateChange
+                                           }
+                                  });
+
+    youTubePlayer.personalPlayer = {'currentTimeSliding': false,
+                                    'errors': []};
+}
+
+
+function youTubePlayerActive() {
+    'use strict';
+
+    return youTubePlayer && youTubePlayer.hasOwnProperty('getPlayerState');
+}
+
+
+// МОДИФИКАЦИЯ ПРОИГРЫВАНИЯ СЛЕД ВИДЕО С СОХРАНЕНИЕМ ТАЙМКОДА ПРЕДЫДУЩЕГО!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function youTubePlayerChangeVideoId() {
+    'use strict';
+
+    var inputVideoId = document.getElementById('YouTube-video-id');
+    var videoId = inputVideoId.value;
+    var curTimeCode = youTubePlayer.getCurrentTime();
+    youTubePlayer.cueVideoById({suggestedQuality: 'tiny',
+                                videoId: videoId,
+                                startSeconds:curTimeCode - 1
+                               });
+    
+    youTubePlayer.playVideo();
+    youTubePlayerDisplayFixedInfos();
+}
+
+
+/**
+ * Main
+ */
+(function () {
+    'use strict';
+
+    function init() {
+        // Load YouTube library
+        var tag = document.createElement('script');
+
+        tag.src = 'https://www.youtube.com/iframe_api';
+
+        var first_script_tag = document.getElementsByTagName('script')[0];
+
+        first_script_tag.parentNode.insertBefore(tag, first_script_tag);
+
+    }
+
+
+    if (window.addEventListener) {
+        window.addEventListener('load', init);
+    } else if (window.attachEvent) {
+        window.attachEvent('onload', init);
+    }
+}());
+
+
+
+
